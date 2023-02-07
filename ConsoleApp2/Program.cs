702953ad842661d36bd977bd86559632280203e6
VladaSandul <123377041+VladaSandul@users.noreply.github.com>
2023-02-07 10:26:34 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //int[] omas = new int[8];
+            //for (int i=0;i<8;i++)
+            //{
+
+            //    Console.WriteLine($"omas[{i}]=");
+            //    omas[i]=int.Parse( Console.ReadLine() );
+            //}
+            //for (int i=0;i<8;i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]");
+            //}
+            //Console.ReadKey();
+
+            //Задача 3
+            //int[] omas = new int[6] { -7, 10, 5, 16, -4, 18 };
+            //for(int i=omas.Length-1;i>=0;--i)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+
+            //Задача 4
+            //Random rnd= new Random();
+            //int n = rnd.Next(163, 190);
+            //int[] omas = new int[12];
+            //for(int i=0;i<12;i++)
+            //{
+            //    omas[i] = rnd.Next(163, 190);
+            //}
+            //for (int i=0;i<12;i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]");
+            //}
+            //Console.ReadKey();
+
+
+        }
+    }
+}
