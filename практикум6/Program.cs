60300ec705aa9c2a3b1e568a11dc923bc66eb509
VladaSandul <123377041+VladaSandul@users.noreply.github.com>
2023-03-06 14:45:43 +0200

    Add files via upload

---
+++ практикум6/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практикум6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("Ведите размер массива=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //Random rnd = new Random();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    omas[i] = rnd.Next(1, 80);
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //int max = omas[0];
+            //int min = omas[0];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (omas[i] > max)
+            //    {
+            //        max = omas[i];
+            //    }
+            //    if (omas[i] < min)
+            //    {
+            //        min = omas[i];
+            //    }
+            //}
+            //Console.WriteLine($"max={max}\t min={min}");
+            //int p = max;
+            //max = min;
+            //min = p;
+            //Console.WriteLine($"Обмененный max={max}, а min={min}");
+            //Console.ReadKey();
+
+
+            //Задача 2
+            //Random rnd = new Random();
+            //double[] omas = new double[12] { 1.4, 7.3, 9.6, 2.9, 4.2, 8.4, 5.5, 6.6, 3.6, 5.7, 9.9, 4.7 };
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"Обычные элементы: omas[{i}]= {omas[i] + ""}");
+            //}
+            //Array.Sort(omas);
+            //Array.Reverse(omas);
+            //Console.WriteLine();
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"Элементы в порядке убвания: omas[{i}]={omas[i] + ""}");
+            //}
+            //double max = omas[0];
+            //double min = omas[0];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    if (omas[i] > max)
+            //    {
+            //        max = omas[i];
+            //    }
+            //    if (omas[i] < min)
+            //    {
+            //        min = omas[i];
+            //    }
+            //}
+            //double sum = 0;
+            //sum += min + max;
+            //Console.WriteLine($" max={max}\t min={min}\t sum(max+min)={sum}");
+            //Console.ReadKey();
+        }
+    }
+}
